a8773a59220f55dfee6f980590b29a66236e34c0
Termux <Harshjangrap@gmail.com>
2026-02-15 21:09:54 +0530

    Adapt Kernel path and config for Lineage Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,22 @@ AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER := true
 TARGET_BOOTLOADER_BOARD_NAME := lisa
 
 # Kernel
-TARGET_KERNEL_CONFIG += lifesteal_lisa_defconfig
+TARGET_KERNEL_CONFIG += vendor/lisa_QGKI.config
+
+# Kernel modules
+BOOT_KERNEL_MODULES := \
+    adsp_loader_dlkm.ko \
+    apr_dlkm.ko \
+    goodix_core.ko \
+    hwid.ko \
+    mmhardware_sysfs_dlkm.ko \
+    msm_drm.ko \
+    q6_notifier_dlkm.ko \
+    q6_pdr_dlkm.ko \
+    qti_battery_charger_main.ko \
+    snd_event_dlkm.ko \
+    xiaomi_touch.ko
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
 
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
